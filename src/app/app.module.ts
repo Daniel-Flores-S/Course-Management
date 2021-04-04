5f587e72673596143a575cca0c52facef1c0d29b
Daniel <danielfloresda802@gmail.com>
2021-04-03 22:09:13 -0300

    Criando componentes com atributos interdependentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { CourseListComponent } from './courses/course-list.component';
+import { StarComponent } from './star/star.component';
+import { ReplacePipe } from './pipe/replace.pipe';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { Error404Compoennt } from './error-404/error-404.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourseListComponent,
+    StarComponent,
+    ReplacePipe,
+    NavBarComponent,
+    Error404Compoennt,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: "", redirectTo: "courses", pathMatch: "full"
+      },
+      {
+        path: "courses", component: CourseListComponent
+      },
+      {
+        path: '**', component: Error404Compoennt
+      }
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
